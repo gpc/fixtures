ea9ed8dfc28499bb605e50ebe850e0604d1cff9d
Luke Daley <ld@ldaley.com>
2008-10-11 07:01:59 +0000

    A plugin to support data fixtures that utilises the Spring DSL to define the fixture

---
+++ src/groovy/grails/fixture/FixtureLoaderRuntimeSpringConfiguration.groovy
@@ -0,0 +1,31 @@
+package grails.fixture
+
+import org.codehaus.groovy.grails.commons.spring.DefaultRuntimeSpringConfiguration
+import org.springframework.beans.factory.config.BeanPostProcessor
+import org.springframework.context.ApplicationContext
+import org.springframework.context.support.GenericApplicationContext
+
+class FixtureLoaderRuntimeSpringConfiguration extends DefaultRuntimeSpringConfiguration {
+
+    public FixtureLoaderRuntimeSpringConfiguration(ApplicationContext parent, ClassLoader classLoader) {
+        super(parent, classLoader);
+    }
+    
+    protected GenericApplicationContext createApplicationContext(ApplicationContext parent) {
+        def ctx = super(parent)
+        ctx.beanFactory.addBeanPostProcessor(
+            [
+                postProcessBeforeInitialization: { Object bean, String beanName ->
+                    bean
+                },
+                postProcessAfterInitialization: { Object bean, String beanName ->
+                    if (!bean.save(flush: true)) {
+                        throw new Error("failed to save fixture bean $beanName")
+                    }
+                    bean
+                },
+            ] as BeanPostProcessor
+        )
+        ctx
+    }
+}
