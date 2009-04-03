e432e30c501e8d56215172557c9dd9f6c6d7ee66
Luke Daley <ld@ldaley.com>
2009-04-03 17:43:41 +0000

    Hold the applicationContext for use by getProperty()

--- src/groovy/grails/fixture/FixtureLoader.groovy
+++ src/groovy/grails/fixture/FixtureLoader.groovy
@@ -3,28 +3,27 @@ import org.springframework.context.ApplicationContextAware
 import org.springframework.context.ApplicationContext
 
 class FixtureLoader implements ApplicationContextAware {
-    
-    def classLoader
+
     ApplicationContext applicationContext
-    
+
     def createBuilder() {
         new FixtureBuilder(applicationContext, classLoader)
     }
-    
+
     void load(String[] fixtures) {
         def bb = createBuilder()
         fixtures.each {
             bb.loadBeans("file:fixtures/${it}.groovy")
         }
-        bb.createApplicationContext()
+        applicationContext = bb.createApplicationContext()
     }
-    
+
     void load(Closure beans) {
         def bb = createBuilder()
         bb.beans(beans)
-        bb.createApplicationContext()
+        applicationContext = bb.createApplicationContext()
     }
-    
+
     def getProperty(name) {
         applicationContext.getProperty(name) ?: super.getProperty(name)
     }
