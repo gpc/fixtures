9e405ffad0cc76fa7334a6f431ca3079c10ecb97
Luke Daley <ld@ldaley.com>
2009-10-18 13:05:52 +1000

    New version: 1.0-SNAPSHOT

--- FixturesGrailsPlugin.groovy
+++ FixturesGrailsPlugin.groovy
@@ -1,7 +1,7 @@
 import grails.plugin.fixtures.FixtureLoader
 
 class FixturesGrailsPlugin {
-    def version = "0.9-RC4"
+    def version = "1.0-SNAPSHOT"
     def dependsOn = [:]
 
     def author = "Luke Daley"
