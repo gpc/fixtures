3cd4185a0a32309af7df7b5f054c9f0739f25384
alkemist <ld@ldaley.com>
2009-11-13 11:54:16 +1000

    Don't try and refresh transient objects.

--- src/groovy/grails/plugin/fixtures/builder/FixtureBuilder.groovy
+++ src/groovy/grails/plugin/fixtures/builder/FixtureBuilder.groovy
@@ -7,6 +7,9 @@ import org.springframework.context.ApplicationContext
 import org.codehaus.groovy.grails.commons.spring.RuntimeSpringConfiguration
 import org.springframework.beans.factory.config.RuntimeBeanReference
 
+import org.hibernate.LockMode
+import org.hibernate.TransientObjectException
+
 class FixtureBuilder extends AbstractFixtureBuilder {
     
     public FixtureBuilder(ApplicationContext parent,ClassLoader classLoader) {
@@ -20,11 +23,20 @@ class FixtureBuilder extends AbstractFixtureBuilder {
     public ApplicationContext createApplicationContext() {
         def ctx = super.createApplicationContext()
 		def grailsApplication = ctx.getBean("grailsApplication")
+        def sessionFactory = ctx.getBean("sessionFactory")
+        
         ctx.beanDefinitionNames.each {
             try {
                 def bean = ctx.getBean(it)
-                if (grailsApplication.isDomainClass(bean.class))
-                    bean.refresh()
+                if (grailsApplication.isDomainClass(bean.class)) {
+                    try {
+                        // We are merely verifying that the object is not transient here
+                        sessionFactory.currentSession.lock(bean, LockMode.NONE)
+                        bean.refresh()
+                    } catch (TransientObjectException e) {
+                        // do nothing
+                    }
+                }
             }
             catch(Exception e) {
                 throw new FixtureException("Error refresh()ing bean '$it'", e)
