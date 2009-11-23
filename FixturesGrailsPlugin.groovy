e9f2082446d2eb12e587b85faf8e9e56230d844c
alkemist <ld@ldaley.com>
2009-11-23 11:45:08 +1000

    Bumped version to 1.0-M2-SNAPSHOT

--- FixturesGrailsPlugin.groovy
+++ FixturesGrailsPlugin.groovy
@@ -1,7 +1,7 @@
 import grails.plugin.fixtures.FixtureLoader
 
 class FixturesGrailsPlugin {
-    def version = "1.0-M1"
+    def version = "1.0-M2-SNAPSHOT"
     def dependsOn = [:]
     def grailsVersion = "1.1 > *"
 
