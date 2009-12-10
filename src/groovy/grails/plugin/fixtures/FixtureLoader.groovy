2a1d5008d76cf110d86e216846618731587abd16
Luke Daley <ld@ldaley.com>
2009-12-10 19:23:58 +1000

    Removed.

--- src/groovy/grails/plugin/fixtures/FixtureLoader.groovy
+++ src/groovy/grails/plugin/fixtures/FixtureLoader.groovy
@@ -5,11 +5,30 @@ import grails.plugin.fixtures.support.AbstractFixtureLoader
 import org.springframework.context.ApplicationContextAware
 import org.springframework.context.ApplicationContext
 
-class FixtureLoader extends AbstractFixtureLoader implements ApplicationContextAware {
+class FixtureLoader implements ApplicationContextAware {
 
     ApplicationContext applicationContext
     
+    private namedFixtures = [:]
+    
     def createFixture() {
         new Fixture(applicationContext)
+    }
+    
+    def load(String[] fixtures) {
+        doLoad(*fixtures)
+    }
+    
+    def load(Closure fixture) {
+        doLoad(fixture)
+    }
+    
+    protected doLoad(Object[] fixtures) {
+        createFixture().load(*fixtures)
+    }
+    
+    def propertyMissing(String name) {
+        if (!namedFixtures.containsKey(name)) namedFixtures[name] = doLoad()
+        namedFixtures[name]
     }
 }
