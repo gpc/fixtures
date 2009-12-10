cbd29ab64945f607d79130863e6d24b9fcc8e71b
Luke Daley <ld@ldaley.com>
2009-12-10 19:21:07 +1000

    Removed.

--- src/groovy/grails/plugin/fixtures/builder/FixtureBuilderRuntimeSpringConfiguration.groovy
+++ src/groovy/grails/plugin/fixtures/builder/FixtureBuilderRuntimeSpringConfiguration.groovy
@@ -2,15 +2,33 @@ package grails.plugin.fixtures.builder
 
 import org.springframework.context.ApplicationContext
 
-import grails.plugin.fixtures.support.builder.AbstractFixtureBuilderRuntimeSpringConfiguration
+import org.codehaus.groovy.grails.commons.spring.DefaultRuntimeSpringConfiguration
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor
+import org.springframework.beans.factory.support.AbstractBeanDefinition
+import org.springframework.context.support.GenericApplicationContext
 
-class FixtureBuilderRuntimeSpringConfiguration extends AbstractFixtureBuilderRuntimeSpringConfiguration {
 
-    public FixtureBuilderRuntimeSpringConfiguration(ApplicationContext parent, ClassLoader classLoader) {
+class FixtureBuilderRuntimeSpringConfiguration extends DefaultRuntimeSpringConfiguration {
+
+    FixtureBuilderRuntimeSpringConfiguration(ApplicationContext parent, ClassLoader classLoader) {
         super(parent, classLoader)
     }
         
     def createBeanPostProcessor(parentCtx) {
         new FixtureBeanPostProcessor(parentCtx: parentCtx)
+    }
+    
+    protected GenericApplicationContext createApplicationContext(ApplicationContext parent) {
+        def ctx = super.createApplicationContext(parent)
+        ctx.addBeanFactoryPostProcessor([
+            postProcessBeanFactory: { beanFactory ->
+                beanFactory.beanDefinitionNames.each {
+                    beanFactory.getBeanDefinition(it).autowireMode = AbstractBeanDefinition.AUTOWIRE_BY_NAME
+                }
+            }] as BeanFactoryPostProcessor
+        )
+        ctx.beanFactory.addBeanPostProcessor(createBeanPostProcessor(parent))
+        
+        ctx
     }
 }
