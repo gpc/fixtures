873594d49c9c4eaf966cf38e940061229aaa9841
Luke Daley <ld@ldaley.com>
2009-12-16 19:43:14 +1000

    Refactored autowirer?

--- src/groovy/grails/plugin/fixtures/builder/FixtureBuilderRuntimeSpringConfiguration.groovy
+++ src/groovy/grails/plugin/fixtures/builder/FixtureBuilderRuntimeSpringConfiguration.groovy
@@ -1,34 +1,22 @@
 package grails.plugin.fixtures.builder
 
 import org.springframework.context.ApplicationContext
-
 import org.codehaus.groovy.grails.commons.spring.DefaultRuntimeSpringConfiguration
-import org.springframework.beans.factory.config.BeanFactoryPostProcessor
-import org.springframework.beans.factory.support.AbstractBeanDefinition
 import org.springframework.context.support.GenericApplicationContext
+
 import grails.plugin.fixtures.builder.processor.FixtureBeanPostProcessor
+import grails.plugin.fixtures.builder.processor.FixtureBeanAutoAutowireConfigurer
 
 class FixtureBuilderRuntimeSpringConfiguration extends DefaultRuntimeSpringConfiguration {
 
     FixtureBuilderRuntimeSpringConfiguration(ApplicationContext parent, ClassLoader classLoader) {
         super(parent, classLoader)
     }
-        
-    def createBeanPostProcessor(parentCtx) {
-        new FixtureBeanPostProcessor(parentCtx: parentCtx)
-    }
-    
+
     protected GenericApplicationContext createApplicationContext(ApplicationContext parent) {
         def ctx = super.createApplicationContext(parent)
-        ctx.addBeanFactoryPostProcessor([
-            postProcessBeanFactory: { beanFactory ->
-                beanFactory.beanDefinitionNames.each {
-                    beanFactory.getBeanDefinition(it).autowireMode = AbstractBeanDefinition.AUTOWIRE_BY_NAME
-                }
-            }] as BeanFactoryPostProcessor
-        )
-        ctx.beanFactory.addBeanPostProcessor(createBeanPostProcessor(parent))
-        
+        ctx.addBeanFactoryPostProcessor(new FixtureBeanAutoAutowireConfigurer())
+        ctx.beanFactory.addBeanPostProcessor(new FixtureBeanPostProcessor(parentCtx: parent))
         ctx
     }
 }
---
+++ src/groovy/grails/plugin/fixtures/builder/processor/FixtureBeanAutoAutowireConfigurer.groovy
@@ -0,0 +1,15 @@
+package grails.plugin.fixtures.builder.processor
+
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory
+
+class FixtureBeanAutoAutowireConfigurer implements BeanFactoryPostProcessor {
+
+    void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) {
+        beanFactory.beanDefinitionNames.each {
+            beanFactory.getBeanDefinition(it).autowireMode = ConfigurableListableBeanFactory.AUTOWIRE_BY_NAME
+        }
+    }
+
+}
+
