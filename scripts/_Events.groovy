71265e81331aef9aa3d9a55769b403d9b4ffaf52
Luke Daley <ld@ldaley.com>
2010-07-10 15:18:04 +1000

    Added local fix for doc aliasing (GRAILS-6484)

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -1,3 +1,5 @@
+import grails.doc.DocEngine
+
 eventWarStart = { warName ->
 	
 	def stagingFixtures
@@ -11,5 +13,12 @@ eventWarStart = { warName ->
 	ant.mkdir(dir: stagingFixtures)
 	ant.copy(todir: stagingFixtures) {
 		fileset(dir: "${grailsSettings.baseDir}/fixtures")
+	}
+}
+
+eventRefdocsStart = {
+	if (grailsAppName == 'grails-fixtures') {
+		createConfig()
+		DocEngine.ALIAS.putAll(config.grails.doc.alias)
 	}
 }
