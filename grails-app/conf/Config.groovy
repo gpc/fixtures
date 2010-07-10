2ef4d9a7ffb69b56f1856d14e0d06104046c320c
Luke Daley <ld@ldaley.com>
2010-07-10 14:13:09 +1000

    Added doc config.

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -1,3 +1,13 @@
 log4j = {
 	debug 'grails.plugin.fixtures'
 }
+
+grails {
+	doc {
+		title = "Grails Fixtures Plugin"
+		subtitle = "Load complex domain data via a convenient DSL"
+		authors = "Grails Plugin Collective"
+		copyright = "Copies of this document may be made for your own use and for distribution to others, provided that you do not charge any fee for such copies and further provided that each copy contains this Copyright Notice, whether distributed in print or electronically."
+		footer = "Developed by the <a href='http://gpc.github.com'>Grails Plugin Collective</a>"
+	}
+}
