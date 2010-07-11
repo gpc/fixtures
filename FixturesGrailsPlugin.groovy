1744dfa5d28dd3d22be65d5faf2577d90e99dc92
Luke Daley <ld@ldaley.com>
2010-07-11 21:55:25 +1000

    -- 1.0

--- FixturesGrailsPlugin.groovy
+++ FixturesGrailsPlugin.groovy
@@ -16,7 +16,7 @@
 import grails.plugin.fixtures.FixtureLoader
 
 class FixturesGrailsPlugin {
-	def version = "1.0-SNAPSHOT"
+	def version = "1.0"
 	def dependsOn = [:]
 	def grailsVersion = "1.1 > *"
 
