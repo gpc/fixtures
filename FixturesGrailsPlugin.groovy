b147990031cf24292824779807f40f4904d4d13e
Luke Daley <ld@ldaley.com>
2011-02-17 09:20:15 +1000

    Update version number.

--- FixturesGrailsPlugin.groovy
+++ FixturesGrailsPlugin.groovy
@@ -16,7 +16,7 @@
 import grails.plugin.fixtures.FixtureLoader
 
 class FixturesGrailsPlugin {
-	def version = "1.0.4"
+	def version = "1.0.5-SNAPSHOT"
 	def dependsOn = [:]
 	def grailsVersion = "1.1 > *"
 
