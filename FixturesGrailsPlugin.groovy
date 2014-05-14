5598826c0c1677548ff551a4a383656c99ef76c4
graemerocher <graeme.rocher@gmail.com>
2014-05-14 18:48:06 +0200

    Update to 1.3.1 snapshot

--- FixturesGrailsPlugin.groovy
+++ FixturesGrailsPlugin.groovy
@@ -16,7 +16,7 @@
 import grails.plugin.fixtures.FixtureLoader
 
 class FixturesGrailsPlugin {
-	def version = "1.3"
+	def version = "1.3.1-SNAPSHOT"
 	def grailsVersion = "1.3 > *"
 
 	def author = "Grails Plugin Collective"
